ac1ef45a62ed822a849d0fef0b7a8bc57481ca0d
Valeriia Shubina <valeriiashbn@gmail.com>
2024-02-09 01:20:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+        int discount = 20;
+
+        int bonusMiles = price/discount;
+        System.out.println("Бонусных миль за билет: " + bonusMiles);
+
+
+    }
+}
+
